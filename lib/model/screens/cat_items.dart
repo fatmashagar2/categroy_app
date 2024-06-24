374dc9c3235d20d820a741cf12bdc77ded163f67
FatmaShagar <fatmashagar64@gmail.com>
2024-06-24 15:01:02 +0300

    cat_app

---
+++ lib/model/screens/cat_items.dart
@@ -0,0 +1,63 @@
+import 'package:category_app/model/category_model.dart';
+import 'package:flutter/material.dart';
+
+class category_items extends StatelessWidget {
+  const category_items({
+    super.key,
+    required this.categories,
+  });
+
+  final List<Category> categories;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 16.0,
+        mainAxisSpacing: 16.0,
+      ),
+      itemCount: categories.length,
+      itemBuilder: (context, index) {
+        final category = categories[index];
+        return Container(
+          child: Card(
+            elevation: 4.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Image.network(
+                    category.thumbnailUrl,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                Text(
+                  category.name,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 8.0),
+                Text(
+                  overflow: TextOverflow.ellipsis,
+                  //maxLines: 2,
+                  category.description.substring(0, 50),
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+
+                    fontSize: 16.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
